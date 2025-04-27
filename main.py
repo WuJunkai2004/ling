1feeff63fcda309a037f05e50d00bdda93d5f67f
Wu Junkai <wu.junkai@qq.com>
2025-04-27 14:40:15 +0800

    feat: 重构主界面和登录界面，添加新窗口布局

--- main.py
+++ main.py
@@ -1,13 +1,69 @@
 import sys
-#PyQt5中使用的基本控件都在PyQt5.QtWidgets模块中
-from PyQt5.QtWidgets import QApplication, QMainWindow
-#导入designer工具生成的login模块
-from units.login import Ui_Login
 
-class MyMainForm(QMainWindow, Ui_Login):
-    def __init__(self, parent=None):
-        super(MyMainForm, self).__init__(parent)
-        self.setupUi(self)
+from qfluentwidgets import NavigationItemPosition, FluentWindow, SubtitleLabel, setFont
+from qfluentwidgets import FluentIcon as FIF
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+from units.login import Ui_Frame as Ui_Login
+
+#import QFrame and QHBoxLayout from PyQt5.QtWidgets
+from PyQt5.QtWidgets import QFrame, QHBoxLayout
+#import AlignCenter from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt
+#import QIcon from PyQt5.QtGui
+from PyQt5.QtGui import QIcon
+
+from units.login import Ui_Frame as LoginFrame
+
+
+class Widget(QFrame):
+    def __init__(self, text: str, parent=None, Frame: QFrame = None):
+        super().__init__(parent=parent)
+        if Frame is None:
+            self.label = SubtitleLabel(text, self)
+            self.hBoxLayout = QHBoxLayout(self)
+            setFont(self.label, 24)
+            self.label.setAlignment(Qt.AlignCenter)
+            self.hBoxLayout.addWidget(self.label, 1, Qt.AlignCenter)
+        else:
+            ui = Frame()
+            ui.setupUi(self)
+
+        # 必须给子界面设置全局唯一的对象名
+        self.setObjectName(text.replace(' ', '-'))
+
+
+class MyMainForm(FluentWindow):
+    """ 主界面 """
+
+    def __init__(self):
+        super().__init__()
+
+        # 创建子界面，实际使用时将 Widget 换成自己的子界面
+        self.homeInterface = Widget('Home Interface', self, LoginFrame)
+        self.musicInterface = Widget('Music Interface', self)
+        self.videoInterface = Widget('Video Interface', self)
+        self.settingInterface = Widget('Setting Interface', self)
+        self.albumInterface = Widget('Album Interface', self)
+        self.albumInterface1 = Widget('Album Interface 1', self)
+
+        self.initNavigation()
+        self.initWindow()
+
+    def initNavigation(self):
+        self.addSubInterface(self.homeInterface, FIF.HOME, 'Home')
+        self.addSubInterface(self.musicInterface, FIF.MUSIC, 'Music library')
+        self.addSubInterface(self.videoInterface, FIF.VIDEO, 'Video library')
+        self.navigationInterface.addSeparator()
+        self.addSubInterface(self.albumInterface, FIF.ALBUM, 'Albums', NavigationItemPosition.SCROLL)
+        self.addSubInterface(self.albumInterface1, FIF.ALBUM, 'Album 1', parent=self.albumInterface)
+        self.addSubInterface(self.settingInterface, FIF.SETTING, 'Settings', NavigationItemPosition.BOTTOM)
+
+    def initWindow(self):
+        self.resize(900, 700)
+        self.setWindowIcon(QIcon(':/qfluentwidgets/images/logo.png'))
+        self.setWindowTitle('灵犀摘')
+
 
 if __name__ == "__main__":
     #固定的，PyQt5程序都需要QApplication对象。sys.argv是命令行参数列表，确保程序可以双击运行
