027188a2f36c5a7c34a392c1aeba5b973f6bd9fc
Wu Junkai <wu.junkai@qq.com>
2025-05-08 10:11:23 +0800

    feat: 更新主界面子界面，添加具体框架以增强功能

--- main.py
+++ main.py
@@ -40,8 +40,8 @@ class MainWin(FluentWindow):
         super().__init__()
         self.navigationInterface.setExpandWidth(250)
         # 创建子界面，实际使用时将 Widget 换成自己的子界面
-        self.homeInterface = Widget('Home Interface', self)
-        self.openInterface = Widget('Open Interface', self)
+        self.homeInterface = Widget('Home Interface', self, Frame_Home)
+        self.openInterface = Widget('Open Interface', self, Frame_Open)
         self.albumInterface = Widget('Album Interface', self)
         self.albumInterface1 = Widget('Album Interface 1', self)
         self.settingInterface = Widget('Setting Interface', self)
