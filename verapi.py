870b973479259cd42f933aa33ffdeb83050aa735
WuJunkai2004 <wujunkai20041123@outlook.com>
2025-05-10 02:04:35 +0800

    feat: 新增启动脚本和API处理程序，添加.gitignore文件

---
+++ verapi.py
@@ -0,0 +1,48 @@
+from imp  import load_source
+from http import server
+
+import os
+import vercel
+
+def Start(handler = vercel.API, port = 8000):
+    server.test(
+        HandlerClass = handler,
+        ServerClass = server.ThreadingHTTPServer,
+        port = port,
+        bind = None
+    )
+
+
+class handler(vercel.API):
+    def vercel(self, url, data, headers):
+        print('url === '+ url)
+        if(os.path.isdir(url)):
+            self.send_code(200)
+            for home in ['index.html','index.htm']:
+                if(os.path.isfile(url + home)):
+                    self.send_file(url + home)
+                    return
+            self.send_text( '\n'.join(os.listdir(url)) )
+            return
+    
+        if(os.path.isfile(url)):
+            if(os.path.splitext(url)[1]=='.py'):
+                vercel.ErrorStatu(self, 403)
+                return
+            self.send_code(200)
+            self.send_file(url)
+            return
+
+        if(os.path.isfile(url + '.py')):
+            mod = load_source(url,url + '.py')
+            try:
+                mod.handler.vercel(self, url, data, headers)
+            except AttributeError:
+                vercel.ErrorStatu(self, 503)
+            return
+        
+        vercel.ErrorStatu(self, 404)
+
+
+if(__name__=='__main__'):
+    Start( handler )
