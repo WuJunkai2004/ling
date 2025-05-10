647aed261c01e3152f1b89d9beda20d1271ed5d3
WuJunkai2004 <wujunkai20041123@outlook.com>
2025-05-10 11:47:00 +0800

    feat: 添加PDF文件上传功能，计算文件MD5并存储信息到数据库，新增dblite数据库操作类

--- api/upload.py
+++ api/upload.py
@@ -1,30 +1,62 @@
+import hashlib
+import io
+import json
+import os
+
+import dblite
 import vercel
+
+def get_md5(file_content: io.BytesIO) -> str:
+    """
+    Calculate the MD5 hash of the given file content. Just count the first 8k bytes.
+    @ brief: file_content: A BytesIO object containing the file content.
+    @ return: The MD5 hash of the file content as a hexadecimal string.
+    """
+    md5_hash = hashlib.md5()
+    chuck = file_content.read(8192)
+    md5_hash.update(chuck)
+    return md5_hash.hexdigest()
+
 
 @vercel.register
 def handler(self: vercel.API, url, data, headers):
     """
     Handles the POST request to upload a PDF file.
-    
-    Args:
-        self: The Vercel request object.
-        url: The URL to which the request is sent.
-        headers: The headers for the request.
-        data: The data to be sent in the request.
-    
-    Returns:
-        A JSON response with the status and message.
     """
     # Check if the request method is POST
     if self.method != 'POST':
         return vercel.ErrorStatu(self, 405)
+    if len(data) != 1:
+        return vercel.ErrorStatu(self, 400)
 
-    # Check if the content type is multipart/form-data
-    print(data)
-    for file in data:
-        if not file['Content']:
-            continue
-        # file['Content'] is the BytesIO object
-        # save it to test.pdf
-        with open('test.txt', 'wb') as f:
-            f.write(file['Content'].read())
-    vercel.ErrorStatu(self, 200)
+    file = data[0]
+    md5  = get_md5(file['Content'])
+    db   = dblite.SQL('./var/datas.db')
+    find = db['files']['md5'].index(md5)
+    if find != -1:
+        # File already exists, 
+        # return the json with the file name and the md5 and file token
+        file_info = db['files'][find]
+        self.send_code(200)
+        self.send_headers({
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        })
+        self.send_text(json.dumps({
+            'filename': file_info['filename'],
+            'md5': md5,
+            'token': md5,
+        }))
+        return
+    file["Content"].seek(0)
+    # 拼接对应的后缀名
+    path = os.path.join('./var/files', md5 + os.path.splitext(file['filename'])[1])
+    with open(path, 'wb') as f:
+        self.copyfile(file['Content'], f)
+    # 记录文件信息
+    db['files'].insert({
+        'filename': md5 + os.path.splitext(file['filename'])[1],
+        'md5': md5,
+    })
+    db.commit()
+    db.close()
---
+++ dblite.py
@@ -0,0 +1,123 @@
+# Author: WuJunkai2004
+# Update: 2025-05-10
+# Version: 1.1.1
+
+
+import sqlite3
+try:
+    import pandas as pd
+except:
+    __can_be_visualized = False
+else:
+    __can_be_visualized = True
+
+debug   = False
+defined = [':memory:', 'debug.db'][debug]
+
+
+class CONLUMN:
+    def __init__(self, cursor, table, conlumn):
+        self.cursor = cursor
+        self.table  = table
+        self.name   = conlumn
+
+    def __setitem__(self, __id, __value):
+        self.insert(__id, __value)
+
+    def __getitem__(self, __id):
+        if(__id < 1):
+            raise RuntimeError("下标错误")
+        self.cursor.execute("SELECT {} FROM {} WHERE oid={}".format( self.name, self.table, __id))
+        return self.cursor.fetchone()[0]
+
+    def update(self, __id, __value):
+        self.insert(__id, __value)
+
+    def insert(self, __id, __value):
+        self.cursor.execute('UPDATE {} set "{}"="{}" WHERE oid={}'.format(self.table, self.name, __value, __id))
+
+    def index(self, __value, __start = 0, __stop = 0x7fffffffffffffff):
+        #return list.index(self, __value, __start, __stop) + 1
+        self.cursor.execute("SELECT oid FROM {} WHERE {}='{}'".format(self.table, self.name, __value))
+        if found := self.cursor.fetchone():
+            return found[0]
+        return -1
+    
+    def append(self, __iterable):
+        for item in __iterable:
+            self.cursor.execute("INSERT INTO {} ({}) VALUES ({})".format(self.table, self.name, '"{}"'.format(item) ) )
+    
+    def __iter__(self):
+        self.cursor.execute("SELECT {} FROM {}".format(self.name, self.table))
+        for item in self.cursor.fetchall().__iter__():
+            yield item[0]
+
+
+class TABLE:
+    def __init__(self, cursor, table) -> None:
+        self.cursor = cursor
+        self.name   = table
+
+    def __getitem__(self, __name):
+        return CONLUMN(self.cursor, self.name, __name)
+
+    def create(self, *__conlumns):
+        self.cursor.execute( 'CREATE TABLE {}\n({});'.format(self.name, ',\n'.join(['"{}" TEXT'.format(item) for item in __conlumns]) ) )
+
+    def insert(self, *__value) -> None:
+        self.cursor.execute("INSERT INTO {} VALUES({})".format(self.name, ",".join(['"{}"'.format(item) for item in __value]) ) )
+
+    def filder(self, __filder):
+        pass
+
+    def del_line(self, __id):
+        self.cursor.execute("DELETE FROM {} WHERE oid={}".format(self.name, __id))
+
+
+
+class SQL:
+    def __init__(self, file = defined) -> None: 
+        self.connect = sqlite3.connect(file)
+        self.cursor  = self.connect.cursor()
+
+    def __getitem__(self, __name) -> TABLE:
+        return TABLE(self.cursor, __name)
+        
+    def __del__(self) -> None:
+        self.close()
+
+    def commit(self):
+        self.connect.commit()
+
+    def vacuum(self):
+        self.connect.execute("VACUUM")
+
+    def close(self):
+        self.connect.commit()
+        self.cursor .close()
+        self.connect.close()
+
+
+def _shell():
+    print("欢迎使用 dblite shell")
+    while(True):
+        cmmd = ''
+        line = input('>>>').rstrip()
+        cmmd += line
+        while(line and (line[0] == ' ' or line[-1] == ':')):
+            line = input('...').rstrip()
+            cmmd += '\n' + line
+        try:
+            result = eval(cmmd)
+        except Exception as e1:
+            try:
+                exec(cmmd)
+            except Exception as e2:
+                print("ERROR ! : {}".format(e2))
+        else:
+            if(result != None):
+                print(result)
+
+
+if(__name__ == "__main__"):
+    _shell()
