0dc98c670b7788c8a9f56fa649723dcb3609182c
WuJunkai2004 <wujunkai20041123@outlook.com>
2025-05-12 20:40:21 +0800

    feat: 新增文件存在性检查API，通过token生成md5进行快速验证

---
+++ api/quicheck.py
@@ -0,0 +1,29 @@
+"""
+通过token -> md5，快速确定文件是否存在
+"""
+
+import vercel
+import dblite
+
+@vercel.register
+def handles(response: vercel.API, data):
+    if 'token' not in data:
+        response.send_code(400)
+        response.send_json({
+            "status": False
+        })
+        return
+    md5 = data['token']
+    db  = dblite.SQL('./var/datas.db')
+    idx = db['files']['md5'].find(md5)
+    if idx == -1:
+        response.send_code(200)
+        response.send_json({
+            "status": False
+        })
+    else:
+        response.send_code(200)
+        response.send_json({
+            "status": True
+        })
+    db.close()
