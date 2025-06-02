6db69519fdc964c09c8b728afeaf41437b63a762
WuJunkai2004 <wujunkai20041123@outlook.com>
2025-06-02 23:17:05 +0800

    feat: 添加PDF转换和封面生成功能

--- api/convert.py
+++ api/convert.py
@@ -1,6 +1,51 @@
 import subprocess
 import vercel
 import os
+import fitz
+
+
+def prepare_pdf(token):
+    # 检查 PDF 文件是否存在
+    if os.path.exists('./var/files/' + token + '.pdf'):
+        return True
+    # 若pdf文件不存在，检查是否存在 caj 文件
+    if not os.path.exists('./var/files/' + token + '.caj'):
+        return False
+    # 若 caj 文件存在，转换为 PDF
+    cmd = [
+        'caj2pdf',
+        '--input', './var/files/' + token + '.caj',
+        '--output', './var/files/' + token + '.pdf'
+    ]
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    if process.returncode != 0:
+        return False
+    return True
+
+
+def prepare_cover(token):
+    # 检查封面图片是否存在
+    if os.path.exists('./var/html/' + token + '.png'):
+        return True
+    # 若封面图片不存在，生成封面
+    pdf_path = './var/files/' + token + '.pdf'
+    # 宽度最多为 240
+    pdf_docs = fitz.open(pdf_path)
+    if pdf_docs.page_count == 0:
+        return False
+    pdf_page = pdf_docs[0]
+    # 获取页面的宽度和高度
+    width = pdf_page.rect.width
+    height = pdf_page.rect.height
+    need_rotate = width < height
+    if need_rotate:
+        width, height = height, width
+    # 计算缩放比例
+    scale = 240 / width
+    # 生成封面图片
+    pix = pdf_page.get_pixmap(matrix=fitz.Matrix(scale, scale))
+    pix.save('./var/cover/' + token + '.png')
 
 
 @vercel.register
@@ -12,6 +57,9 @@ def convert(response: vercel.API, data):
     if response.method != 'POST':
         return vercel.ErrorStatu(response, 405)
     if not os.path.exists('./var/html/' + data['token'] + '.html'):
+        if not prepare_pdf(data['token']):
+            return vercel.ErrorStatu(response, 500)
+        prepare_cover(data['token'])
         cmd = [
             'pdf2htmlEX',
             '--fit-width', '800',
--- api/trans.py
+++ api/trans.py
@@ -1,4 +1,45 @@
 import vercel
+import requests
+import random
+import json
+import hashlib
+
+url: str = "http://api.fanyi.baidu.com/api/trans/vip/translate"
+appid = '20250602002372171'
+appkey = 'tTwWnkfrs8HUcuA_c6M6'
+
+
+def make_md5(s, encoding='utf-8'):
+    return hashlib.md5(s.encode(encoding)).hexdigest()
+
+
+def translate(text):
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    salt = random.randint(32768, 65536)
+    sign = make_md5(appid + text + str(salt) + appkey)
+    payload = {
+        'appid': appid,
+        'q': text,
+        'from': 'en',
+        'to': 'zh',
+        'salt': salt,
+        'sign': sign
+    }
+    try:
+        response = requests.post(url, params=payload, headers=headers)
+    except:
+        return ''
+    if response.status_code != 200:
+        return ''
+    try:
+        result = response.json()
+    except:
+        return ''
+    ret = ''
+    for i in result['trans_result']:
+        ret += i['dst']
+    return ret
+
 
 def reply(response, success, text):
     response.send_code(200)
@@ -9,7 +50,7 @@ def reply(response, success, text):
     
 
 @vercel.register
-def handler(response, data):
+def handler(response: vercel.API, data):
     """
     translate text using google translate api
     should get a data like
@@ -23,4 +64,4 @@ def handler(response, data):
         return reply(response, False, "Method not allowed")
     if not data or "token" not in data or "text" not in data:
         return reply(response, False, "Invalid data")
-    pass
+    return reply(response, True, translate(data["text"]))
